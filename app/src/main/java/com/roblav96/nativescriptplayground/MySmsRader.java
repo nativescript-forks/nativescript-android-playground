c52a9731cd28987c0c441933a467c219b9cb7fd2
Rob Laverty <roblav96@gmx.com>
2016-10-25 10:44:53 -0400

    it dupes

--- app/src/main/java/com/roblav96/nativescriptplayground/MySmsRader.java
+++ app/src/main/java/com/roblav96/nativescriptplayground/MySmsRader.java
@@ -3,22 +3,27 @@ package com.roblav96.nativescriptplayground;
 import android.content.Context;
 import android.util.Log;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.tuenti.smsradar.Sms;
 import com.tuenti.smsradar.SmsListener;
 import com.tuenti.smsradar.SmsRadar;
 
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * Created by roblav96 on 10/25/16.
@@ -30,70 +35,109 @@ public class MySmsRader {
     private static final String TAG = "roblav96";
 
 
-
-    private Context _context;
     private String _url;
-    private RequestQueue _queue;
     private ArrayList<String> _failed;
+    private String _headers;
+
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    private OkHttpClient _client;
 
     public MySmsRader(
             Context context,
-            String IP
+            String ip,
+            String headers
     ) {
-        _context = context;
-        _url = IP + "/api/android-sms";
-        _queue = Volley.newRequestQueue(_context);
+        _url = ip + "/api/android-sms";
+        _headers = headers;
         _failed = new ArrayList<>();
+        _client = new OkHttpClient();
 
         SmsRadar.initializeSmsRadarService(context, new SmsListener() {
             @Override
             public void onSmsSent(Sms sms) {
-                Log.e(TAG, "onSmsSent > sms > " + sms);
-                sendSms(sms.toString());
+//                Log.e(TAG, "onSmsSent > sms > " + sms);
+                sendSms(sms);
             }
             @Override
             public void onSmsReceived(Sms sms) {
-                Log.e(TAG, "onSmsReceived > sms > " + sms);
-                sendSms(sms.toString());
+//                Log.e(TAG, "onSmsReceived > sms > " + sms);
+                sendSms(sms);
             }
         });
     }
 
-    public void stopService() {
-        SmsRadar.stopSmsRadarService(_context);
+    public void stopService(Context context) {
+        SmsRadar.stopSmsRadarService(context);
     }
 
-    private void sendSms(final String sms) {
-        StringRequest request = new StringRequest(
-            Request.Method.POST,
-            _url,
-            new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    Log.e(TAG, "onResponse > response > " + response);
+    private void sendSms(Sms _sms) {
+        Gson gson = new Gson();
+        final String sms = gson.toJson(_sms);
+
+        ArrayList<String> sendi = _failed;
+        sendi.add(sms);
+
+        Request request = new Request.Builder()
+        .url(_url)
+        .post(RequestBody.create(JSON, gson.toJson(sendi)))
+        .build();
+
+        _client.newCall(request).enqueue(new Callback() {
+            @Override public void onFailure(Call call, IOException e) {
+                _failed.add(sms);
+            }
+            @Override public void onResponse(Call call, Response response) throws IOException {
+                Log.e(TAG, "onResponse > isSuccessful > " + response.isSuccessful());
+                Log.e(TAG, "onResponse > code > " + response.code());
+                if (response.isSuccessful()) {
                     _failed.clear();
-                }
-            },
-            new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Log.e(TAG, "onErrorResponse > error > " + error.getMessage());
+                } else {
                     _failed.add(sms);
                 }
             }
-        ){
-            @Override
-            protected Map<String,String> getParams() {
-                Map<String, String> params = new HashMap<>();
-                params.put("item0", sms);
-                for (int i = 0; i < _failed.size(); i++) {
-                    params.put("item" + (i + 1), _failed.get(i));
-                }
-                return params;
-            }
-        };
-        _queue.add(request);
+        });
     }
+
+
+
+
+
+//    private void sendSms_old(final String sms) {
+//        StringRequest request = new StringRequest(
+//                Request.Method.POST,
+//                _url,
+//                new Response.Listener<String>() {
+//                    @Override
+//                    public void onResponse(String response) {
+//                        Log.e(TAG, "onResponse > response > " + response);
+//                        _failed.clear();
+//                    }
+//                },
+//                new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        Log.e(TAG, "onErrorResponse > error > " + error.getMessage());
+//                        _failed.add(sms);
+//                    }
+//                }
+//        ){
+//            @Override
+//            protected Map<String,String> getParams() {
+//                Map<String, String> params = new HashMap<>();
+//                params.put("item0", sms);
+//                for (int i = 0; i < _failed.size(); i++) {
+//                    params.put("item" + (i + 1), _failed.get(i));
+//                }
+//                return params;
+//            }
+//
+//            @Override
+//            public Map<String,String> getHeaders() {
+//                return _headers;
+//            }
+//        };
+//        _queue.add(request);
+//    }
 
 }
 
